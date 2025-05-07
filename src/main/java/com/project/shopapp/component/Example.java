4618603407c0419580fd41b4226c3f5107e8bae6
vuongducthoai <thoai12309@gmail.com>
2025-05-07 14:11:38 +0700

    CON-5 add test

--- src/main/java/com/project/shopapp/component/Example.java
+++ src/main/java/com/project/shopapp/component/Example.java
@@ -3,6 +3,7 @@ package com.project.shopapp.component;
 public class Example {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123");
         System.out.println("abc");
         System.out.println("Hello, world!");
     }
